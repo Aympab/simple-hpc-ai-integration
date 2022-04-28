6786ba0896f3a092a6fe57ad83f21d6e354746ae
Aymeric Millan <Aymeric.Millan@cea.fr>
2022-04-28 13:56:59 +0200

    get final result

--- main.cpp
+++ main.cpp
@@ -27,9 +27,22 @@ using Eigen::MatrixXf;
 using Eigen::VectorXf;
 using Eigen::Map;
 using Eigen::Matrix;
+using Eigen::Vector;
 
 
 #define __DEBUG 1
+
+
+void inline print_buffer(std::string infos, float* buffer, size_t N){
+  std::cout << "\n\nBUFFER " << infos << "=\n";
+  for (size_t i = 0; i<N; ++i){
+    std::cout << buffer[i];
+    if(i < N-1)
+      std::cout << ", ";
+    else
+      std::cout << std::endl;
+  }
+}
 
 int main (int argc, char* argv[]){
   /* Variables initialization */
@@ -58,7 +71,7 @@ int main (int argc, char* argv[]){
   } // end if(myid == 0)
 
 
-  //Sending M size to everyone
+  //Sending Matrix size to everyone
   MPI_Bcast(&N, 1, my_MPI_SIZE_T, 0, MPI_COMM_WORLD);
 
   /* Split & Send matrix between the procs */
@@ -67,8 +80,11 @@ int main (int argc, char* argv[]){
   //Settings arrays to use to split matrix with MPI
   int counts[num_procs];
   int displacements[num_procs];
+  int counts_final[num_procs];
+  int displacements_final[num_procs];
 
   displacements[0] = 0;
+  displacements_final[0] = 0;
   //Filling the counts and offset arrays for MPI_ScatterV
   if(myid == 0)
   {
@@ -78,6 +94,7 @@ int main (int argc, char* argv[]){
       int plower;
       int pupper;
       int sum_count = 0;
+      int sum_count_final = 0;
 
       for(int p = 0; p < num_procs; p++){
           plower=0;
@@ -90,10 +107,17 @@ int main (int argc, char* argv[]){
           pupper += std::min(p+1, pextra);
 
           counts[p] = (pupper - plower) * N;
+          counts_final[p] = (pupper - plower);
 
-          if(p > 0) displacements[p] = sum_count;
+          if(p > 0){
+            displacements[p] = sum_count;
+            displacements_final[p] = sum_count_final;
+            // std::cout << displacements_final[p] <<
+          }
           sum_count += counts[p];
+          sum_count_final += counts_final[p];
       } // end for p
+
   } // end if(myid == 0)
 
   if(N % num_procs != 0){
@@ -105,10 +129,9 @@ int main (int argc, char* argv[]){
     }
   }// end if(N % num_procs != 0)
 
-  // if(__DEBUG)
-      // std::cout << "#" << myid << " - local size = " << localNbRows << std::endl;
-
-  float values[localNbRows*N];
+  float valuesBuff[localNbRows*N];
+  // float* vecBuff = (float*)malloc(N*sizeof(float));
+  float vecBuff[N];
 
   if(myid == 0){
     /*
@@ -123,42 +146,90 @@ int main (int argc, char* argv[]){
     std::mt19937 gen(rd());
     std::uniform_real_distribution<float> uniform(-1.0, 1.0);
 
-    // MatrixXf M = MatrixXf::NullaryExpr(N, N,[&](){return uniform(gen);});
-    MatrixXf M = MatrixXf::Random(N, N);
-    VectorXf v = VectorXf::NullaryExpr(N,[&](){return uniform(gen);});
-    if (__DEBUG){
-      std::cout << "M =" << std::endl << M << std::endl;
-    //   std::cout << "v =" << std::endl << v << std::endl;
-    }
+    Matrix<float, -1, -1, Eigen::RowMajor> M = MatrixXf::
+                                                NullaryExpr(
+                                                  N, N,
+                                                  [&](){return uniform(gen);}
+                                                );
+    Vector<float, -1> v = VectorXf::NullaryExpr(N,[&](){return uniform(gen);});
 
-  /* Send matrix to other proc */
+    // std::cout << "REAL VEC =\n" << v << std::endl;
+
+    /* Send matrix to other proc */
     MPI_Scatterv(
         M.data(),         //buffer_send
         counts,           //number of elements to send to each proc
         displacements,    //displacement to each process
         MPI_FLOAT,        //type send
-        &values,          //buffer to receive values
+        &valuesBuff,          //buffer to receive valuesBuff
         localNbRows*N,    //number of elements on the reveive buffer
         MPI_FLOAT,        //datatype receive
         0,                //root MPI ID
         MPI_COMM_WORLD);
+
+    /* Setup to send the whole vector, going to use this in Bcast */
+    // vecBuff = v.data(); //NO IDEA WHY JUST THIS DOESN'T WORK ????
+    auto tmp = v.data();
+    for(size_t i =0; i<N;i++){
+      vecBuff[i] = tmp[i];
+    }
   }
   else{
     MPI_Scatterv(NULL, NULL, NULL, MPI_FLOAT,
-        &values,
+        &valuesBuff,
         localNbRows*N,
         MPI_FLOAT,        //Datatype receive
         0,                //root MPI ID
         MPI_COMM_WORLD);
   } // end if(myid == 0)
 
-  auto localMat = Eigen::Map<MatrixXf>(values, localNbRows, N);
-  if(myid == 1){
+  // if(myid == 0) print_buffer(std::string("BEFORE BCAST"), vecBuff, N);
 
-    std::cout << "LOCAL MAT=" << localMat << std::endl;
+  /* Receiving objects */
+  //Build local matrix
+  auto localMat = Eigen::Map<MatrixXf>(valuesBuff, localNbRows, N);
+
+  //Bcast the whole vector into vecBuffer
+  MPI_Bcast(&vecBuff, N, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  
+  //Create Eigen vector based on vecBuffer
+  auto localVec = Eigen::Map<VectorXf>(vecBuff, N);
+
+  //Compute partial Mv product
+  VectorXf localRes = localMat * localVec;
+  // if(myid == 0 || myid == 1) std::cout << "\n\nLOCAL RES=\n"<< localRes << std::endl;
+
+  if(myid == 0){
+    float finalBuff[N];
+    MPI_Gatherv(localRes.data(),  //const void* buffer_send,
+                localNbRows,      //int count_send,
+                MPI_FLOAT,        //MPI_Datatype datatype_send,
+                &finalBuff,        //void* buffer_recv,
+                counts_final,           //const int* counts_recv,
+                displacements_final,    //const int* displacements,
+                MPI_FLOAT,        //MPI_Datatype datatype_recv,
+                0,                //int root,
+                MPI_COMM_WORLD);  //MPI_Comm communicator);
+
+
+  VectorXf finalResult = Eigen::Map<VectorXf>(finalBuff, N);
+  std::cout << "\n\nFINAL RES =\n"<< finalResult << std::endl;
+
+  }
+  else{
+    MPI_Gatherv(localRes.data(),  //const void* buffer_send,
+                localNbRows,      //int count_send,
+                MPI_FLOAT,        //MPI_Datatype datatype_send,
+                NULL,             //void* buffer_recv,
+                NULL,             //const int* counts_recv,
+                NULL,             //const int* displacements,
+                MPI_FLOAT,        //MPI_Datatype datatype_recv,
+                0,                //int root,
+                MPI_COMM_WORLD);  //MPI_Comm communicator);
   }
 
   /* Finalize MPI */
   MPI_Finalize();
   return 0;
 }
+
